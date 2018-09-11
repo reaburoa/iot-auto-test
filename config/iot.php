72dda3f9ce12ac3f1e4e697482d8d21be25ec8d5
Rean <zl@sunmi.com>
2018-09-11 11:50:39 +0800

    yh

--- config/iot.php
+++ config/iot.php
@@ -15,19 +15,19 @@ return [
         ]
     ],
     'mario' => [
-        'host' => env('MARIO_EMQTT_HOST', 'mqtt://uatmario.sunmi.com:81'),
+        'host' => env('MARIO_EMQTT_HOST', 'mqtt://47.99.116.111:1883'),
         'data_client' => [
-            'client_id' => 'marioServerData',
+            'client_id' => 'marioServerData-auto-test',
             "username" => 'marioServerData',
             'password' => 'marioServerData',
         ],
         'active_client' => [
-            'client_id' => 'marioServerActive',
+            'client_id' => 'marioServerActive-auto-test',
             "username" => 'marioServerActive',
             'password' => 'marioServerActive',
         ],
         'watchdog_client' => [
-            'client_id' => 'marioServerWatchdog',
+            'client_id' => 'marioServerWatchdog-auto-test',
             "username" => 'marioServerWatchdog',
             'password' => 'marioServerWatchdog',
         ]
